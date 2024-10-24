b95512239aeedd64730072d345078ebfcd9f6086
KaranyiczGabor <karanyiczg@kkszki.hu>
2024-10-24 08:56:08 +0200

    Database is ready

---
+++ BlogAPI/BlogAPI/Models/BlogDBContext.cs
@@ -0,0 +1,28 @@
+﻿using BlogAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Asn1.Mozilla;
+
+namespace UserAPI.Models
+{
+    public class BlogDBContext : DbContext
+    {
+        public BlogDBContext() { }
+
+        public BlogDBContext(DbContextOptions opitions) : base(opitions)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string conn = "server=localhost; database=users; user=root; password=";
+
+                optionsBuilder.UseMySQL(conn);
+            }
+        }
+
+        public DbSet<User> NewUsers { get; set; } = null!;
+    }
+}
---
+++ BlogAPI/BlogAPI/Models/DTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogAPI.Models
+{
+    public class DTO
+    {
+        public record CreateUserDTO(string Title, string Description, DateTime CreatedTime, DateTime LastUpdated);
+        public record UpdateUserDTO(string Title, string Description, DateTime CreatedTime, DateTime LastUpdated);
+
+    }
+}
